bec2555e61028697f3ccfeff2503b9a4abe6d21c
Mohammed Al Abri <striker.m901@gmail.com>
2025-07-20 10:24:30 +0400

    feat: Initial project setup for ScientificCalculator

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ScientificCalculator {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static void displayMenu() {
+        
+
+
+    }
+
 }
